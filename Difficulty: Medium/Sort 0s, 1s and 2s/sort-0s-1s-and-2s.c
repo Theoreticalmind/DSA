c994d77b916bb3f122252b883e811aec583c6186
Md Osama Ali <103421846+Theoreticalmind@users.noreply.github.com>
2026-01-05 21:34:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.c
@@ -0,0 +1,24 @@
+void sort012(int arr[], int n) {
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            int temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {  // arr[mid] == 2
+            int temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+    }
+}

f0d2cea31fa872c5b83dd16ae631ffddb19e12f2
Md Osama Ali <103421846+Theoreticalmind@users.noreply.github.com>
2026-05-18 17:05:36 +0530

    Time: 19 ms (17.34%) | Memory: 21.2 MB (11.85%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = r = 0
+        n = len(height)
+        max_l = [0] * n
+        max_r = [0] * n
+
+        for i in range(n):
+            j = -i-1
+            max_l[i] = l
+            max_r[j] = r
+            l = max(l, height[i])
+            r = max(r, height[j])
+
+        sum = 0
+        for i in range(n):
+            pot = min(max_l[i], max_r[i])
+            sum += max(0,   pot - height[i])
+            
+        return sum
